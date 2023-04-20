707e69516642bed5301ac3051bb662fa300a3384
Rafael <83092886+rafaelsnk@users.noreply.github.com>
2023-04-20 14:00:38 -0300

    Add files via upload
    Implementação básica da funcionalidade de acender e apagar a lâmpada.

---
+++ lampada/assets/js/principal.js
@@ -0,0 +1,12 @@
+const botaoLigar = document.querySelector('#ligar');
+const botaoDesligar = document.querySelector('#desligar');
+
+botaoLigar.addEventListener('click', function () {
+  const img = document.querySelector('img');
+  img.src = '/assets/img/turnon.png';
+})
+
+botaoDesligar.addEventListener('click', function () {
+  const img = document.querySelector('img');
+  img.src = '/assets/img/turnoff.png';
+})
