e9c5c3851b0ebfc42f17f0a9fe6857078f8e3bd3
Rafael <83092886+rafaelsnk@users.noreply.github.com>
2023-04-25 21:01:54 -0300

    Add files via upload

--- lampada/assets/js/principal.js
+++ lampada/assets/js/principal.js
@@ -1,12 +1,17 @@
 const botaoLigar = document.querySelector('#ligar');
 const botaoDesligar = document.querySelector('#desligar');
+const img = document.querySelector('img');
+
 
 botaoLigar.addEventListener('click', function () {
-  const img = document.querySelector('img');
   img.src = './assets/img/turnon.png';
 })
 
 botaoDesligar.addEventListener('click', function () {
-  const img = document.querySelector('img');
   img.src = './assets/img/turnoff.png';
+})
+
+img.addEventListener('click', function () {
+  img.src = './assets/img/broken.png';
+
 })
